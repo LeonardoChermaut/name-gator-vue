06b5de5cbd945eba400d197c9cfda290f471e9ee
Leonardo Chermaut <chermauts@hotmail.com>
2023-09-14 08:43:30 -0300

    chore: vue updated version 3

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,35 @@ import "font-awesome/css/font-awesome.css";
 import DomainListComponent from "./components/DomainList.vue";
 
 export default {
-	name: "app",
-	components: {
-		DomainListComponent,
-	},
+  name: "app",
+  components: {
+    DomainListComponent,
+  },
 };
 </script>
 
 <template>
-	<div>
-		<div id="slogan" class="text-center">
-			<h1>Verificador de Domínios</h1>
-			<br />
-			<h6 class="text-secondary">Stacks: Vue.js, GraphQL e Node 😃</h6>
-		</div>
-		<div id="main">
-			<DomainListComponent />
-		</div>
-	</div>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>Verificador de Domínios</h1>
+      <br />
+      <h6 class="text-secondary">Stacks: Vue.js, GraphQL e Node 😃</h6>
+    </div>
+    <div id="main">
+      <DomainListComponent />
+    </div>
+  </div>
 </template>
 
 <style>
 #slogan {
-	margin-top: 30px;
-	margin-bottom: 30px;
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 
 #main {
-	background-color: #f1f1f1;
-	padding-top: 30px;
-	padding-bottom: 30px;
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
 }
 </style>
